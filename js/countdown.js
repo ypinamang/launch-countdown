01b8022862c0f3d91c347b980b977b594d927b1b
Yaw <ypinamangyapong@gmail.com>
2025-07-24 15:31:56 +0900

    Add countdown functionality

---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+const ONE_SECOND = 1000;
+const counter = document.querySelector(".counter");
+
+
+function getCounterValue(counter) {
+    return parseInt(counter.textContent);
+}
+
+const numberInSeconds = getCounterValue(counter);
+
+function pause(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds));
+}
+
+async function performCountDown() {
+    for (let i = numberInSeconds; i > 0; i--) {
+        counter.textContent = i;
+        await pause(ONE_SECOND);
+    }
+}
+
+performCountDown();
+
+
+
+
+
+
