dee686d9618dbbff50076397bf22bf3a3a7aedc5
Yaw <ypinamangyapong@gmail.com>
2025-07-24 16:01:23 +0900

    Add clap sound when countdown ends

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,10 @@
 const ONE_SECOND = 1000;
 const counter = document.querySelector(".counter");
 
+const clapSound = document.querySelector("#clap-sound");
+
+counter.addEventListener("click", performCountDown);
+
 
 function getCounterValue(counter) {
     return parseInt(counter.textContent);
@@ -16,10 +20,12 @@ async function performCountDown() {
     for (let i = numberInSeconds; i > 0; i--) {
         counter.textContent = i;
         await pause(ONE_SECOND);
+        
     }
+    clapSound.play();
 }
 
-performCountDown();
+
 
 
 
